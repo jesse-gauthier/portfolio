798e09d19dae01fb517c1bb7ea6efe06db079224
jesse-gauthier <jessegauthier2@gmail.com>
2024-07-14 14:36:09 -0400

    "Updated Password_Generator.vue to replace React Contact Book content with Password Generator content"

--- src/pages/projects/Password_Generator.vue
+++ src/pages/projects/Password_Generator.vue
@@ -4,41 +4,34 @@
 	>
 		<div class="max-w-4xl mx-auto">
 			<h2 class="text-2xl md:text-3xl font-bold mb-4 text-center">
-				React Contact Book
+				Password Generator
 			</h2>
 
 			<div class="flex flex-col md:flex-row md:space-x-4">
 				<img
-					src="/src/assets/projects/contact_book/contact_book3.png"
+					src="/src\assets\projects\password_generator\password_generator1.png"
 					alt="Screenshot of the React Contact Book application"
 					class="w-full md:w-1/2 h-auto rounded-lg mb-4 md:mb-0"
-				/>
-				<img
-					src="/src/assets/projects/contact_book/contact_book1.png"
-					alt="Another screenshot of the React Contact Book application"
-					class="w-full md:w-1/2 h-auto rounded-lg"
 				/>
 			</div>
 
 			<p class="text-base md:text-lg my-4">
-				The React Contact Book application showcases the powerful capabilities
-				of React, utilizing a custom Google FireStore Database for seamless data
-				storage. This project was entirely developed by me as an academic
-				assignment, where I achieved a perfect grade of 100%.
+				Developed a robust password generator using pure JavaScript, featuring a
+				click-to-copy functionality for user convenience. The tool ensures
+				strong password creation with a user-friendly interface, enhancing
+				security and usability for diverse applications.
 			</p>
 
 			<div>
 				<h3 class="text-xl md:text-2xl font-semibold mb-2">App Features</h3>
 				<ul class="list-disc list-inside space-y-2">
-					<li>React</li>
-					<li>FireStore</li>
+					<li>Vanilla JS - No Libraries</li>
+					<li>Local Storage</li>
 					<li>Tailwind</li>
-					<li>Vite</li>
+					<li>Click To Copy</li>
 					<li>
 						The ability to toggle dark/light modes upon the press of a button
 					</li>
-					<li>Create and update new contacts</li>
-					<li>View previously stored contacts</li>
 				</ul>
 			</div>
 		</div>
