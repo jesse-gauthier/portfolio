a79012e0f5921a0f1868902237e2890aeee305b8
jesse-gauthier <jessegauthier2@gmail.com>
2024-07-14 17:06:39 -0400

    "Added back button and live version link to GottagoOttawa.vue, and rearranged layout"

--- src/pages/projects/GottagoOttawa.vue
+++ src/pages/projects/GottagoOttawa.vue
@@ -2,6 +2,18 @@
 	<div
 		class="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200 p-4 md:p-8 rounded-xl"
 	>
+		<div class="flex justify-between">
+			<router-link class="btn btn-warning w-100" to="/projects"
+				>Back To Projects</router-link
+			>
+			<a
+				class="btn bg-[#2b3d4f] text-white hover:bg-[#e89e1d]"
+				href="https://thewebmasters.online/"
+				target="_blank"
+				>Live Version</a
+			>
+		</div>
+		<div class="bg-[#e0ffff] block h-[1px] my-8"></div>
 		<div class="max-w-4xl mx-auto">
 			<div class="flex flex-wrap flex-row-reverse justify-evenly">
 				<h2 class="text-3xl md:text-5xl font-bold mb-4 text-center self-center">
@@ -13,12 +25,7 @@
 					class="w-[80%] md:w-1/2 lg:w-1/6 h-auto rounded-lg mb-4 md:mb-0"
 				/>
 			</div>
-			<a
-				class="mt-5 btn bg-[#2b3d4f] text-white hover:bg-[#e89e1d]"
-				href="https://thewebmasters.online/"
-				target="_blank"
-				>Live Version</a
-			>
+
 			<p class="text-base md:text-lg my-4">
 				Gotta Go Ottawa's new website was my capstone project in the final
 				semester of my Interactive Media Program. As the team second in a group
